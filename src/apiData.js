ee965aa515ff180115e025a180195507635d47bd
Tendai <tendai1@hotmail.com>
2021-08-04 13:33:01 +0200

    create the API class

--- src/apiData.js
+++ src/apiData.js
@@ -1,1 +1,32 @@
-const
+import { import } from 'components'
+class ApiData {
+  constructor(http) {
+    this.http = http;
+  }
+
+  createGame = async (name) => {
+    const {
+      result,
+    } = await post('games', {
+      name,
+    });
+    const id = parseId(result);
+    return id;
+  };
+
+  fetchScores = async (id) => {
+    const {
+      result,
+    } = await get(`games/${id}/scores/`);
+    return result;
+  };
+
+  submitScore = async (id, data) => {
+    const {
+      result,
+    } = await post(`games/${id}/scores/`, data);
+    return result;
+  };
+}
+
+export default ApiData;
--- src/components.js
+++ src/components.js
@@ -1,1 +1,3 @@
 const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const parseId = asyc (result );
