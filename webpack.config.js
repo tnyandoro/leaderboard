762030c6d1104bdfb97a5c56f9692479f31da4a3
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-05 12:56:34 +0200

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
         },
       },
       {
